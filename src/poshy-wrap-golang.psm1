94937d2fc6d627f564717b04e635d746c62e809a
Bruce Markham <219281+brucificus@users.noreply.github.com>
2024-12-16 20:37:13 -0500

    ⬆️Updates Dependency 'poshy-env-var'

--- src/poshy-wrap-golang.psm1
+++ src/poshy-wrap-golang.psm1
@@ -1,7 +1,7 @@
 #!/usr/bin/env pwsh
 $ErrorActionPreference = "Stop"
 Set-StrictMode -Version Latest
-#Requires -Modules @{ ModuleName = "poshy-env-var"; RequiredVersion = "0.5.25" }
+#Requires -Modules @{ ModuleName = "poshy-env-var"; RequiredVersion = "0.6.0" }
 #Requires -Modules @{ ModuleName = "poshy-lucidity"; RequiredVersion = "0.4.1" }
 
 
